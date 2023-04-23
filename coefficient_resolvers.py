cab8e6c2173bbc2b160e687272ebb32ef26426f9
Andryss <andr.krivosheev@gmail.com>
2023-04-23 12:52:22 +0300

    Add coefficients and change finding method

--- coefficient_resolvers.py
+++ coefficient_resolvers.py
@@ -16,24 +16,39 @@ _student_distribution_quantiles: dict[int, dict[float, float]] = {
     1: {
         0.750: 1.000
         # ...
+    },
+    14: {
+        0.975: 2.145
     }
     # ...
 }
+
+
+def _get_nearest_key(d: dict, k: float):
+    for k_cur in d.keys():
+        if abs(k - k_cur) < 1e-5:
+            return k_cur
+    return None
 
 
 def student_distribution_coefficient_resolver(n: int, p: float) -> float:
     if n not in _student_distribution_quantiles:
         raise Exception(f"Student coefficients for n={n} haven't added yet")
     ps = _student_distribution_quantiles[n]
-    if p not in ps:
+    nearest_key = _get_nearest_key(ps, p)
+    if nearest_key is None:
         raise Exception(f"Student coefficient for n={n} and p={p} hasn't added yet")
-    return ps[p]
+    return ps[nearest_key]
 
 
 _pearson_distribution_quantiles: dict[int, dict[float, float]] = {
     1: {
         0.005: 0.0000393
         # ...
+    },
+    14: {
+        0.025: 5.63,
+        0.975: 26.1
     }
     # ...
 }
@@ -43,6 +58,7 @@ def pearson_distribution_coefficient_resolver(n: int, p: float) -> float:
     if n not in _pearson_distribution_quantiles:
         raise Exception(f"Pearson coefficients for n={n} haven't added yet")
     ps = _pearson_distribution_quantiles[n]
-    if p not in ps:
+    nearest_key = _get_nearest_key(ps, p)
+    if nearest_key is None:
         raise Exception(f"Pearson coefficient for n={n} and p={p} hasn't added yet")
-    return ps[p]
+    return ps[nearest_key]

e75cc48c3ed23c037dadcad79d6606066f721ecd
Andryss <andr.krivosheev@gmail.com>
2023-04-23 15:28:38 +0300

    Change sources and printing

--- main.py
+++ main.py
@@ -4,20 +4,35 @@ from series_holder import *
 from confidence_interval_calculator import *
 
 
-def print_interval_info(interval_info: ConfidenceIntervalInfo):
-    print(interval_info.sample_mean)
+def print_interval_info(series_info: SeriesInfo, interval_info: ConfidenceIntervalInfo):
+    print(f"\n----------RESULT----------")
+    print(f"{series_info}:")
+    print(f"\nLength: {interval_info.length}")
+    print(f"\nSample mean: {interval_info.sample_mean}")
+    print(f"Interval: ({interval_info.sample_mean_interval[0]},{interval_info.sample_mean_interval[1]})")
+    print(f"\nSample variance: {interval_info.sample_variance}")
+    print(f"Interval: ({interval_info.sample_variance_interval[0]},{interval_info.sample_variance_interval[1]})")
+    print(f"\nSample deviation: {interval_info.sample_deviation}")
 
 
-def run_1():
+def run_with_series():
     series_info = SeriesInfoWithSourceSeries()
-    series_info.source_series = get_task1_task7_numbers()
-    interval_info = calculate_confident_interval(series_info, 0.95)
-    print_interval_info(interval_info)
+    series_info.source_series, upsilon = get_test_task_numbers()
+    interval_info = calculate_confident_interval(series_info, upsilon)
+    print_interval_info(series_info, interval_info)
+
+
+def run_with_info():
+    series_info = SeriesInfoWithCharacteristics()
+    (series_info.length, series_info.series_sum, series_info.series_squares_sum), upsilon = get_test_task_info()
+    interval_info = calculate_confident_interval(series_info, upsilon)
+    print_interval_info(series_info, interval_info)
 
 
 def run():
     try:
-        run_1()
+        run_with_series()
+        run_with_info()
     except Exception as e:
         print(e, file=sys.stderr)
 
--- series_holder.py
+++ series_holder.py
@@ -1,13 +1,29 @@
 import numpy as np
 
 
-def get_task1_task7_numbers() -> np.ndarray:
-    return np.array([
-        79, 86, 87, 90, 82, 88, 73, 91, 94, 92, 100, 83, 96, 89, 78
-    ])
-
-
-def get_task2_task7_info() -> (int, float, float):
+def get_test_task_numbers() -> (np.ndarray, float):
     return (
-        80, 69, 4
+        np.array([162, 151, 161, 170, 167, 164, 166, 164, 173, 172]),
+        0.95
+    )
+
+
+def get_task1_task7_numbers() -> (np.ndarray, float):
+    return (
+        np.array([79, 86, 87, 90, 82, 88, 73, 91, 94, 92, 100, 83, 96, 89, 78]),
+        0.95
+    )
+
+
+def get_test_task_info() -> ((int, float, float), float):
+    return (
+        (72, 1267.2, 22536),
+        0.99
+    )
+
+
+def get_task2_task7_info() -> ((int, float, float), float):
+    return (
+        (80, 69, 4),
+        0.99
     )
